99007f9a93b3e3aaec5ba222856f96ac8cb9b29d
Sarah Okoro <sarahokoro12@gmail.com>
2022-10-14 23:09:20 +0000

    Hexadecimal

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: to 0
+ */
+
+int main(void)
+{
+	char g;
+
+	char h;
+
+	g = 'a';
+	while
+		(h < 10)
+		{
+			putchar(h + '0');
+			h++;
+		}
+	while
+		(g <= 'f')
+		{
+			putchar(g);
+			g++;
+		}
+	putchar('\n');
+	return (0);
+}
