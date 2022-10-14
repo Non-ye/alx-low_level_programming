87bcbafbca4930f81dd65bcd904f397f6f79a040
Sarah Okoro <sarahokoro12@gmail.com>
2022-10-14 23:34:11 +0000

    Patience, persistence and perspiration make an unbeatable combination for success

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: to 0
+ */
+
+int main(void)
+{
+	int a;
+
+	a = 0;
+	while
+		(a < 100)
+		{
+			putchar(a + '0');
+			putchar(',');
+			putchar(' ');
+			a++;
+		}
+	putchar('\n');
+	return (0);
+}
