9189051dc097fe42cb71a14557f4aca93e641d79
Sarah Okoro <sarahokoro12@gmail.com>
2022-10-14 00:39:04 +0000

    Hello, puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Entry
- * Return: Always 0
+ * Return: to 0
  */
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle\n");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
 
