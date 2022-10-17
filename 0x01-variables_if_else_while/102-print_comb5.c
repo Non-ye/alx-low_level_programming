b2891d82bee0375a88e765f37e260e92304f4549
Sarah Okoro <sarahokoro12@gmail.com>
2022-10-17 00:24:23 +0000

    Software is eating the World

---
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+/**
+ * main - Entry
+ * Return: to 0
+ */
+
+int main(void)
+{
+	int h;
+	int i;
+
+	for (h = 0; h < 99; h++)
+	{
+		for (i = h + 1; i < 100; i++)
+		{
+			if (h != i && i != h)
+			{
+				putchar((h / 10) + '0');
+				putchar((i / 10) + '0');
+				putchar(' ');
+				putchar((h % 10) + '0');
+				putchar((i % 10) + '0');
+				if (h + i > 1)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
