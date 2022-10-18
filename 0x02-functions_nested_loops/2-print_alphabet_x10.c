3653d916bca03f216c73d42819257e0f401d80d4
Sarah Okoro <sarahokoro12@gmail.com>
2022-10-18 21:57:39 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * print_alphabet_x10 - Entry
+ * Return: Nothing
+ */
+void print_alphabet_x10(void)
+{
+	char i = 0;
+	char j;
+
+	while (i < 10)
+	{
+		j = 'a';
+		while (j <= 'z')
+		{
+			_putchar(j);
+			j++;
+		}
+	}
+	_putchar('\n');
+	i++;
+}
+
