a9d202a9e50954d2ef3b56f2f5cd442a530ae7ee
Sarah Okoro <sarahokoro12@gmail.com>
2022-10-18 20:08:31 +0000

    Alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet - Entry
+ *
+ * Return: Always 0.
+ */
+void print_alphabet(void)
+{
+	int b = 'a';
+
+	while (b <= 'z')
+
+	{
+		_putchar(b);
+		b++;
+	}
+	_putchar('\n');
+}
