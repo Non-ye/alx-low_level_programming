07962d5a4ff67d4da5a06c1ccfe467f2fc022858
Sarah Okoro <sarahokoro12@gmail.com>
2022-10-19 02:33:59 +0000

    Time table

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -1,4 +1,43 @@
 #include "main.h"
 /**
- * print_times_table - 
- * Return 
+ * print_times_table - Prints the n times table
+ * @n: number time table (0 < n <= 15)
+ * Return: Nothing
+ */
+void print_times_table(int n)
+{
+	int a, b, op;
+
+	if (n >= 0 && n <= 15)
+	{
+		for (a = 0; a <= n; a++)
+		{
+			_putchar('0');
+			for (b = 1; b, = n; b++)
+			{
+				op = a * b;
+				_putchar(',');
+				_putchar(' ');
+				if (op <= 9)
+				{
+					_putchar(' ');
+					_putchar(' ');
+					_putchar(op + '0');
+				}
+				else if (op <= 'c')
+				{
+					_putchar(' ');
+					_putchar((op / 10) + '0');
+					_putchar((op % 10) + '0');
+				}
+				else
+				{
+					_putchar(((op / 100) % 10) + '0');
+					_putchar(((op / 10) % 10) + '0');
+					_putchar((op % 10) + '0');
+				}
+			}
+			_putchar('\n');
+		}
+	}
+}
