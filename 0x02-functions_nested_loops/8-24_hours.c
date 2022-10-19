adad88209f7932a7d39d66c0e3f912f3aba5b1db
Sarah Okoro <sarahokoro12@gmail.com>
2022-10-19 01:26:00 +0000

    Jack Bauer

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,30 @@
+#include "main.h"
+/**
+ * jack_bauer - Entry
+ * Return: No return
+ */
+void jack_bauer(void)
+{
+	int e, f, g, h;
+
+	for (e = '0'; e <= '2'; e++)
+	{
+		for (f = '0'; f <= '9'; f++)
+		{
+			for (g = '0'; g <= '5'; g++)
+			{
+				for (h = '0'; h <= '9'; h++)
+				{
+					if (e >= '2' && >= '4')
+						break;
+					_putchar(a);
+					_putchar(b);
+					_putchar(':');
+					_putchar(c);
+					_putchar(d);
+					_putchar('\n');
+				}
+			}
+		}
+	}
+}
