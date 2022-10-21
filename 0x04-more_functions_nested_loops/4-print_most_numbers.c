88b4234a203b256ab2af9b002a35edba575c3ae7
Sarah Okoro <sarahokoro12@gmail.com>
2022-10-21 16:02:02 +0000

    Write a function that prints the numbers, from 0 to 9, followed by a new line and does not print 2 and 4

---
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_most_numbers - Entry
+ * Return: to Nothing
+ */
+
+void print_most_numbers(void)
+{
+	char g = 48;
+
+	while (g <= 57)
+	{
+		if ((g != 50 && g != 52) && g <= 57)
+			_putchar(g);
+		g++;
+	}
+	_putchar('\n');
+}
